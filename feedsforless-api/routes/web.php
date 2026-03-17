1a484179dd7688a2ee31705bd06ae5cdd3814fe5
Josue Daniel Martinez <daniel@allhay.com>
2026-03-17 13:01:55 -0400

    delet route to run migrations

--- feedsforless-api/routes/web.php
+++ feedsforless-api/routes/web.php
@@ -10,10 +10,10 @@ Route::prefix('api')->group(function () {
     require base_path('routes/api.php');
 });
 
-Route::get('/run-migrations', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-    return 'Migrations ran successfully!';
-});
+// Route::get('/run-migrations', function () {
+//     \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+//     return 'Migrations ran successfully!';
+// });
 Route::get('/run-seeders', function () {
     \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
     return 'Seeders ran successfully!';
