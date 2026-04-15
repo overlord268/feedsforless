9401ceb3bec54597551aec5e49cb0d5b3540998a
Josue Daniel Martinez <daniel@allhay.com>
2026-04-15 10:58:34 -0400

    refactor: update production API URLs

--- feedsforless-api/config/cors.php
+++ feedsforless-api/config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_values(array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173'))))),
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://feedsforless-front-production-ab18.up.railway.app',
+],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
--- feedsforless-api/config/sanctum.php
+++ feedsforless-api/config/sanctum.php
@@ -16,11 +16,11 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    '%s%s%s',
+    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+    Sanctum::currentApplicationUrlWithPort(),
+    ',feedsforless-front-production-ab18.up.railway.app'
+))),
 
     /*
     |--------------------------------------------------------------------------
